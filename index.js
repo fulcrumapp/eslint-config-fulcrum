e1cdf3f6af90b94b7e512fc40defea1af9c12aa0
Zac McCormick <zac.mccormick@gmail.com>
2016-05-19 10:20:03 -0400

    Relax no-param-reassign, it causes nearly duplicate variables for no benefit

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ module.exports = {
     "no-obj-calls": 2,
     "no-octal": 2,
     "no-octal-escape": 2,
-    "no-param-reassign": 2,
+    "no-param-reassign": 0,
     "no-path-concat": 2,
     "no-process-env": 0,
     "no-process-exit": 2,
