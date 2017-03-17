1f59e3163a6745de5b7f2d92e1fb6d195d81f674
Zac McCormick <zac.mccormick@gmail.com>
2017-03-17 09:59:56 -0400

    Allow rest vars to be unused for ommitting things

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ module.exports = {
     "no-unreachable": 2,
     "no-unsafe-negation": 2,
     "no-unused-expressions": 2,
-    "no-unused-vars": [2, {"vars": "all", "args": "none"}],
+    "no-unused-vars": [2, {"vars": "all", "args": "none", "ignoreRestSiblings": true}],
     "no-unsafe-finally": 2,
     "no-use-before-define": 2,
     "no-useless-call": 2,
